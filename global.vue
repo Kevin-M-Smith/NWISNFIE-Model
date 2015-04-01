f567549758c44afec44f18b768898584914b10d0
Kevin M. Smith <kevin.smith@tufts.edu>
2015-04-01 11:46:59 -0400

    Initial commit.

---
+++ global.vue
@@ -0,0 +1,101 @@
+<!-- Tufts VUE 3.2.2 concept-map (global.vue) 2015-04-01 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/Applications/VUE.app/Contents/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Wed Apr 01 11:43:58 EDT 2015 by kms on platform Mac OS X 10.10.2 in JVM 1.6.0_65-b14-466.1-11M4716 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0" label="global.vue"
+    created="1427902786427" x="0.0" y="0.0" width="1.4E-45"
+    height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1427903038474"
+        spec="/Users/kms/Desktop/NWISNFIE-Model/global.vue" type="1" xsi:type="URLResource">
+        <title>global.vue</title>
+        <property key="File" value="/Users/kms/Desktop/NWISNFIE-Model/global.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/75a74415c0a8017100cdc97b99f32ea7</URIString>
+    <child ID="6" label="global_config.yaml" layerID="1"
+        created="1427902881689" x="-692.0" y="33.0" width="114.0"
+        height="22.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#F1A83E</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/75a74416c0a8017100cdc97b5af265f9</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="8"
+        label="db:&#xa; host : 127.0.0.1&#xa; name : postgres&#xa; user : postgres&#xa; pass : &quot;&quot;&#xa;&#xa;time:&#xa; utc.offset : -06:00&#xa;&#xa;collections:&#xa; params : &quot;00060,00065&quot;&#xa; lookback : P5W&#xa;&#xa;# netcdf subsets&#xa;netcdf:&#xa;  national : &quot;/data/netcdf/iv/daily/national&quot;&#xa;  # 2 DIGIT HUC&#xa;  huc_l1 : &quot;/data/netcdf/iv/daily/huc_l1&quot;&#xa;  # 4 DIGIT HUC&#xa;  huc_l2 : &quot;/data/netcdf/iv/daily/huc_l2&quot;&#xa;  # 6 DIGIT HUC&#xa;  huc_l3 : &quot;/data/netcdf/iv/daily/huc_l3&quot;&#xa;  # 8 DIGIT HUC&#xa;  huc_l4 : &quot;/data/netcdf/iv/daily/huc_l4&quot;&#xa;  # NFIE-HYDRO&#xa;  nfie_hydro : &quot;/data/netcdf/iv/daily/nfie_hydro&quot;&#xa;&#xa;tables:&#xa; data : data&#xa; staging.prefix : staging_&#xa; site.metadata : site_metadata&#xa; param.metadata : param_metadata&#xa; active.sites : active_sites&#xa; site.assets : site_assets&#xa; param.codes : param_codes&#xa; sensor.metadata : sensor_metadata&#xa; url.stash : url_stash&#xa;&#xa;parallel:&#xa; max.downloaders : 4&#xa; max.builders : 2&#xa;&#xa;logging:&#xa; level : INFO&#xa; time.stamp.format : &quot;[%Y-%m-%d %H:%M:%S]&quot;&#xa; file : NULL&#xa; &#xa;tempdir: &quot;/tmp&quot;"
+        layerID="1" created="1427902935071" x="-213.0" y="72.0"
+        width="268.0" height="680.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#F1A83E</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/75a74422c0a8017100cdc97b86437fde</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1427902786455" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/75a74423c0a8017100cdc97bfb9e6c92</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-1663.5" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1427902786416"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/75a74424c0a8017100cdc97bb0eca4cf</URIString>
+            <masterSlide ID="2" created="1427902786508" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/75a74424c0a8017100cdc97b21a4c684</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1427902786952" x="341.0" y="175.0"
+                    width="118.0" height="50.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/75a74425c0a8017100cdc97b85db782b</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1427902786953" x="349.5" y="282.5"
+                    width="101.0" height="35.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/75a74426c0a8017100cdc97b3135dbde</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1427902786955"
+                    x="375.5" y="385.0" width="49.0" height="30.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/75a74427c0a8017100cdc97b99db5c9e</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2015-04-01</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>/Users/kms/Desktop/NWISNFIE-Model</saveLocation>
+    <saveFile>/Users/kms/Desktop/NWISNFIE-Model/global.vue</saveFile>
+</LW-MAP>
